c673e409afc686bc80e79d6a08c69b5420a90896
Goliaf1992 <109434281+Goliaf1992@users.noreply.github.com>
2022-10-25 16:34:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,183 @@
+
+
+
+
+
+
+class Rectangle {
+    constructor (height,width) {
+
+        this.height = height;
+        this.width = width;
+        
+    }
+}
+
+//extends 
+class ImagesFrame extends Rectangle {
+
+    constructor(height,width,bgImage,parentSelector,borderRadius,...rest) {
+
+        super(height,width); //супер всегда идет первой строчкой дублирует ту часть кода которая была здесь у родителя
+        
+        
+        this.bgImage = bgImage;
+        this.bgSize = 'cover';
+        this.parent = document.querySelector(parentSelector);
+        this.borderRadius = borderRadius;
+
+    }
+
+    
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = `<div></div>`;
+        element.style.height = this.height + 'px';
+        element.style.width = this.width + 'px';
+        element.style.borderRadius =  this.borderRadius + 'px';
+        element.style.backgroundImage = this.bgImage;
+        element.style.backgroundSize = this.bgSize;
+        element.classList.add('img');
+        this.parent.append(element);
+        return element;
+
+    }
+
+}
+
+
+
+//avatar img
+
+let avatar =  new ImagesFrame(300,300,`url(images/23.jpg`,'.info-container',150).render();
+avatar.style.backgroundSize = 'cover';
+avatar.style.backgroundPosition = '';
+
+
+//creates divs for image-container 
+
+let imgURL = ['IMG_0274.jpg','IMG_0275.jpg','IMG_0279.jpg','IMG_0280.jpg','IMG_0281.jpg','IMG_0297.jpg','IMG_0111.jpg','IMG_0145.jpg'];
+let portraitsArr = [];
+
+let imgUrlPeople = ['IMG_0111.jpg','IMG_0113.jpg','IMG_0115.jpg','IMG_0118.jpg','IMG_0119.jpg'];
+let peopleArr = [];
+
+
+function createImgDiv(arr,arr2,parent) {
+
+    for (let i = 0; i<arr.length;i++){
+
+        
+        arr2[i] = new ImagesFrame(400,400,`url(images/${arr[i]}`,`${parent}`,20).render();
+    }
+    
+    }
+
+createImgDiv(imgURL,portraitsArr,'#portraits');
+
+createImgDiv(imgUrlPeople,peopleArr,'#people');
+
+
+
+
+
+function hideImg(selector) { //hide img
+
+
+
+    document.querySelectorAll(selector).forEach(img=>{
+
+    if(img.classList.contains('zoomed')){
+        img.style.display = 'block';
+    }
+
+    else {
+        img.style.display = 'none';
+    }
+    
+});
+}
+
+    //show IMG
+
+function showImg(selector) { 
+    
+    document.querySelectorAll(selector).forEach(img=>{
+
+        img.style.display = 'block';});
+
+ 
+}
+
+
+//zoom img that was clicked and hide others
+
+let count = 0;
+
+document.querySelectorAll('.img-container').forEach(element=>{
+    
+    element.addEventListener('click', function(e) {
+    const tgt = e.target; console.log(tgt);
+    
+        if(tgt.classList.contains('img')){
+            count++;
+            var x = document.createElement('dialog');
+            document.querySelector('.img-container').append(x);
+            x.style.backgroundImage = tgt.style.backgroundImage;
+            x.classList.toggle('photo-modal');
+            x.showModal();
+
+
+            x.addEventListener('click',()=>{
+                x.close();
+                count++;
+                showImg('.img');
+            });
+            
+        }
+        
+});});
+
+
+
+
+
+// modal script
+
+const trigger = document.querySelector('#contact'),
+        modal = document.querySelector('#reach-me'),
+        btnCloseModal = document.querySelector('#close-modal');
+
+trigger.addEventListener('click',(event)=>{
+    event.preventDefault();
+    modal.showModal();
+});
+
+btnCloseModal.addEventListener('click',()=>{
+    modal.close();
+});
+
+
+
+// -----------------locations javascript------------------
+
+let imgUrlLocations = ['Haifa1.webp','moscow.jpg'];
+let locationsArr = [];
+
+
+// ------------------nav-bar-work javascript--------------
+
+
+
+const navElementWork = document.querySelector('#nav-element');
+
+navElementWork.addEventListener('click',(event)=>{
+    event.preventDefault();
+    document.querySelector('.additional-links-wrapper').classList.toggle('active');
+    
+    
+});
+
+
+
